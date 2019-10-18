4566581181d1317d3c0fefdcbe56a26757dc6cca
Anureet-Sidhu <56404939+Anureet-Sidhu@users.noreply.github.com>
2019-10-18 12:47:41 -0400

    Update app.module.ts
    Changed key

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AgmCoreModule } from '@agm/core';
-
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpModule,
+    HttpClientModule,
     AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyBcdTdMBd-wKj2CbaqWf999zaG4KN4UUAI'
+      apiKey: 'your key'
     })
   ],
   providers: [],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
-export class AppModule {}
+export class AppModule {
+  
+}
