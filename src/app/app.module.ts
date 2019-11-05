da8e16c0da0c3991e590ff25e7a2b759b20b1fc8
Anureet-Sidhu <56404939+Anureet-Sidhu@users.noreply.github.com>
2019-11-05 10:12:53 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,29 @@ import { AgmCoreModule } from '@agm/core';
 import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { MapComponent } from './map/map.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     HttpModule,
     HttpClientModule,
+    FormsModule,
+    AngularFontAwesomeModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
     AgmCoreModule.forRoot({
-      apiKey: 'your key'
+      apiKey: 'YOUR GOOGLE KEY'
     })
   ],
   providers: [],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, MapComponent, SideNavComponent, FooterComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {
